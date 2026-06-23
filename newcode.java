30f2c202623e0a58df3223d4e56c60a7bfe90574
MochiRakesh <rs9440230037>
2026-06-23 13:48:18 +0530

    the 2nd new repo

---
+++ newcode.java
@@ -0,0 +1,13 @@
+package newrepo2;
+import java.util.*;
+public class newcode {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the nos");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+
+    }
+}
